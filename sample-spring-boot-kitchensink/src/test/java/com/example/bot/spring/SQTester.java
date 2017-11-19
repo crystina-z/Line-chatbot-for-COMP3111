ca9f69f7f01c1ae6b98f09623cfee257b51ca320
yuanyuan qi <yuanyuanqi777@gmail.com>
2017-11-20 00:08:59 +0800

    SQTester

---
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/SQTester.java
@@ -0,0 +1,41 @@
+package com.example.bot.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.bot.spring.database.RecommendationDBEngine;
+import com.example.bot.spring.database.UQDBEngine;
+import com.example.bot.spring.textsender.*;
+import com.example.bot.spring.database.*;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest(classes = {SQTester.class, TextProcessor.class})
+public class SQTester { 
+	@Autowired
+	private TextProcessor tp;
+	
+	private String testerId="sq_tester";
+
+	@Test
+	public void SQTester() throws Exception {
+		String msgs[]= {"Hello"};
+		for (String msg:msgs) {
+			try{//should fail
+				tp.processText(testerId, msg);
+			}catch(Exception e) {
+				
+			}
+		}
+	}
+}
+
+
